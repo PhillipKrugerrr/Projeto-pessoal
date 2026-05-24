16132226d25029bf44d1c5a40ed3e48abd8f44be
Phillip Kruger <galeradapesada-999@hotmail.com>
2026-05-24 18:23:05 +0200

    Add localStorage for "theme"

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const sections = document.querySelectorAll(".hidden");
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
-    })
+    });
 });
 
 sections.forEach(section => {
@@ -14,12 +14,21 @@ sections.forEach(section => {
 
 const themeBtn = document.getElementById("theme-toggle");
 
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+    themeBtn.textContent = "Light Mode";
+} else {
+    themeBtn.textContent = "Dark Mode";
+}
+
 themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
 
     if (document.body.classList.contains("dark-theme")) {
         themeBtn.textContent = "Light Mode";
+        localStorage.setItem("theme", "dark");
     } else {
         themeBtn.textContent = "Dark Mode";
+        localStorage.setItem("theme", "light");
     }
 });
